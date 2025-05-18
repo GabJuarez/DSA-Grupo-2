ed16e1473c3a20cbd8cafb2394afad8bdeb87e43
GabJuarez <gajuarez@uamv.edu.ni>
2025-05-18 14:57:01 -0600

    corrigiendo ultimo ejercicio

--- Colas/ejercicio1.py
+++ Colas/ejercicio1.py
@@ -8,27 +8,69 @@ estudiantes cómo se evita el desorden en el uso compartido de un recurso limita
 from collections import deque
 
 cola_documentos = deque()
+documento_actual = None  #para el documento que se esta imprimiendo actualmenete
 
 def agregar_documento():
-    seq = int(input('Cuantos documentos desea agregar? '))
-    for i in range(seq):
-        nombre_documento = input('Ingrese el nombre del documento: ')
-        usuario_envio = input('Quien envio el documento: ')
-        numero_pag = int(input('Ingrese el numero de paginas del documento: '))
-        agregar = f'Documento {nombre_documento} numero {i} enviado por: {usuario_envio} con {numero_pag} pagina/s'
-        cola_documentos.append(agregar)
-        
+    try:
+        cantidad = int(input('Ingrese la cantidad de documentos que quiere enviar a la cola de impresión: '))
+        for i in range(cantidad):
+            print(f'\nDocumento {i+1}:')
+            nombre_documento = input('Ingrese el nombre del documento: ')
+            usuario_envio = input('quien envio el documento?: ')
+            numero_pag = int(input('Ingrese el número de páginas del documento: '))
+
+            documento = {
+                'nombre': nombre_documento,
+                'usuario': usuario_envio,
+                'paginas': numero_pag
+            }
+
+            cola_documentos.append(documento)
+            print('documento agregado a la cola correctamente')
+    except ValueError:
+        print("ingresar un numero valido")
+
 def procesar():
-    seq = int(input('Cuantos documentos desea procesar: '))
-    for i in range(seq):
-        print('Procesando')
-        print(f'{cola_documentos[0]}')
-        cola_documentos.popleft()
-    
-def mostrar_cola(agregar):
-    for i in range(len(cola_documentos)):
-        print(agregar)
-        
-    
+    global documento_actual
+    if not cola_documentos:
+        print("No hay documentos en la cola para procesar")
+        documento_actual = None
+        return
+
+    try:
+        cantidad = int(input('Ingrese la cantidad de documentos que desea procesar: '))
+        for i in range(cantidad):
+            if cola_documentos:
+                documento_actual = cola_documentos.popleft()
+                print(f'\nImprimiendo documento: "{documento_actual["nombre"]}"')
+                print(f'Enviado por: {documento_actual["usuario"]}')
+                print(f'Número de páginas: {documento_actual["paginas"]}')
+            else:
+                print("\nNo hay más documentos en la cola.")
+                documento_actual = None
+                break
+    except ValueError:
+        print("Ingrese un numero valido")
+
+def mostrar_cola():
+    if not cola_documentos:
+        print("la cola de impresion esta vacia")
+    else:
+        print("\nDocumentos en la cola de impresión:")
+        for i, doc in enumerate(cola_documentos, start=1):
+            print(f'{i}. Documento: "{doc["nombre"]}", enviado por: {doc["usuario"]}, {doc["paginas"]} página(s)')
+
+def ver_documento_actual():
+    if documento_actual:
+        print("\nDocumento que se está imprimiendo actualmente:")
+        print(f'Documento: "{documento_actual["nombre"]}", enviado por: {documento_actual["usuario"]}, {documento_actual["paginas"]} página(s)')
+    else:
+        print("No se está imprimiendo ningún documento en este momento.")
+
+#simulando
 agregar_documento()
+mostrar_cola()
 procesar()
+ver_documento_actual()
+mostrar_cola()
+
